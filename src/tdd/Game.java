9de9f01b2ef90ad6964960d69ced8636fa0c0eef
Mohamed amine Mekki <mkm4050a@ensfsi.onmicrosoft.com>
2024-04-11 17:37:49 +0200

    bug

---
+++ src/tdd/Game.java
@@ -0,0 +1,18 @@
+package tdd;
+
+public class Game {
+	
+	private int nbTour = 0;
+	private int[]tabRoll = new int[20];
+	
+	void roll(int nbQuilles) {
+		//Nothing
+		
+	}
+	
+	
+	int score() {
+		return 0;
+	}
+
+}
